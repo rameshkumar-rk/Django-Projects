fd8ff16d1f6712861da2ed3b4b153f69782364fd
Rameshkumar A <47273636+rameshkumar-rk@users.noreply.github.com>
2020-07-28 22:13:02 +0530

    Add files via upload

---
+++ wisdompets/adoptions/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Pet(models.Model):
+    S_Choice=[('M','Male'),('F','Female')]
+    name=models.CharField(max_length=100)
+    submitter=models.CharField(max_length=100)
+    species=models.CharField(max_length=30)
+    breed=models.CharField(max_length=30,blank=True)
+    description=models.TextField()
+    s=models.CharField(max_length=1,choices=S_Choice,blank=True)
+    submission_date=models.DateTimeField()
+    age=models.IntegerField(null=True)
+    vaccinations=models.ManyToManyField('Vaccine',blank=True)
+
+class Vaccine(models.Model):
+    name=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
